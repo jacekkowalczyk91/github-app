a9ab3662c0ed32be252e652554b09ac5252dfcf0
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-04-26 18:26:32 +0200

    on progress

--- src/Components/GithubView/GithubView.js
+++ src/Components/GithubView/GithubView.js
@@ -3,28 +3,53 @@ import React from 'react'
 class GithubView extends React.Component {
 
     state = {
-        data: []
+        data: [],
+        lastDate: []
     }
 
     componentDidMount() {
         fetch(`https://api.github.com/users/jacekkowalczyk91/repos`)
             .then(response => response.json())
-            .then(data =>
-                this.setState({data})
-            )
+            .then(data => {
+
+                this.setState({
+                    lastDate: new Date(Math.max(...data.map(data => new Date(data.updated_at)))),
+                    data: data
+                })
+            })
     }
+
+    compareBy = key => {
+        return function (a, b) {
+            if (b[key] < a[key]) return -1;
+            if (b[key] > a[key]) return 1;
+            return 0;
+        };
+    }
+
+    sortBy = key => {
+        let arrayCopy = [...this.state.data];
+        arrayCopy.sort(this.compareBy(key));
+        this.setState({data: arrayCopy});
+    }
+
 
 
     render() {
 
-        const maxDate = this.state.data.map(function (e) {
-            return e.updated_at;
-        }).sort().reverse()[0]
 
+        const {data} = this.state
         return (
             <div>
+                <button
+                    onClick={this.sortBy('updated_at')}
+                >Latest repo name
+                </button>
                 {
-                    maxDate
+                    data.map(
+                        data =>
+                            <p>{data.name}</p>
+                    )
                 }
             </div>
         )
